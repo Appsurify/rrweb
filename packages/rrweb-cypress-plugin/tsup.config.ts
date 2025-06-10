f4b3da8b50390eb7ea1c7354e47de95dd2ce679d
Artem Demidenko <ar.demidenko@appsurify.com>
2025-06-10 17:15:56 +0300

    Refactor rrweb Cypress plugin to dynamically inline `rrweb-record` using custom esbuild plugin and update build scripts

--- packages/rrweb-cypress-plugin/tsup.config.ts
+++ packages/rrweb-cypress-plugin/tsup.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'tsup';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const recordPath = path.resolve(__dirname, '../record/dist/rrweb-record.umd.cjs');
+const recordSource = fs.readFileSync(recordPath, 'utf-8');
 
 export default defineConfig({
   sourcemap: true,
@@ -9,7 +14,22 @@ export default defineConfig({
   noExternal: ['rrweb'],
   entry: ['src/index.ts', 'src/reporter.ts'],
   format: ['cjs', 'esm'], // 'iife'
-  loader: {
-    '.src': 'text',
-  },
+  // loader: {
+  //   '.src': 'text',
+  // },
+  esbuildPlugins: [
+    {
+      name: 'rrweb-record-loader',
+      setup(build) {
+        build.onResolve({ filter: /rrweb-record\.umd\.cjs\.src$/ }, args => ({
+          path: args.path,
+          namespace: 'rrweb-record',
+        }));
+        build.onLoad({ filter: /.*/, namespace: 'rrweb-record' }, () => ({
+          contents: recordSource,
+          loader: 'text',
+        }));
+      },
+    },
+  ],
 });
