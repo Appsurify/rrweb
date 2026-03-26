e0f824b2b3b36dfc2ff36f6eede0498e4abe0141
ar.demidenko <ar.demidenko@gmail.com>
2026-03-26 09:20:02 +0300

    feat(cypress-demo): remove legacy visual coverage demo app
    Removed the entire Visual Coverage Todo app, including HTML, CSS, JavaScript, and Cypress e2e tests to streamline the repository and reduce maintenance overhead.
    
    BREAKING CHANGE: The Visual Coverage demo app and its associated tests are no longer part of the repository.

--- examples/cypress-simple-demo/cypress/support/e2e.ts
+++
@@ -1,9 +0,0 @@
-// ***********************************************************
-// Visual Coverage initialization
-// ***********************************************************
-
-const { initializeTestmap } = require('@appsurify-testmap/rrweb-cypress-plugin')
-initializeTestmap()
-
-// Import custom commands
-import './commands';
